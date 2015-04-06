01dc03caaf0a60eba809b5ce391750caa53abecb
boddmg <boddmg@boddmgs-MacBook-Air.local>
2015-04-06 20:15:41 +0800

    Add the 28.scm.

---
+++ 1/28.scm
@@ -0,0 +1,16 @@
+(load "24.scm")
+(define (expmod base exp m)
+	(cond ((= exp 0) 1)
+				((even? exp)
+				 (remainder 
+					 (square (expmod base (/ exp 2) m))
+					 m))
+				(else
+					(remainder 
+						(* base (expmod base (- exp 1) m))
+						m))))
+
+(define (Miller-Rabin-test n)
+	(define (try-it a)
+		(= (expmod a n n) a))
+	(try-it (+ 1 (random (- n 1)))))
